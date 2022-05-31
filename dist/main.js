f3b66b9569b8741b1ac47ecd581f50a9e9893eac
Eduardo Motta de Moraes <edumoraes@live.com>
2022-05-31 19:58:53 -0300

    initial commit

---
+++ dist/main.js
@@ -0,0 +1,9 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+console.log(`test`)
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7OztBQUFBLG1CIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vdG9wLXByb2plY3QtdG9kby1saXN0Ly4vc3JjL2luZGV4LmpzIl0sInNvdXJjZXNDb250ZW50IjpbImNvbnNvbGUubG9nKGB0ZXN0YCkiXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=
